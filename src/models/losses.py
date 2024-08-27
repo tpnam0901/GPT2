2b6a2df5780f84b186f5b677ebf980771d8bfb97
tpnam0901 <tpnam0901@gmail.com>
2024-08-27 20:29:41 +0900

    fix(losses.py): missing parent init function

--- src/models/losses.py
+++ src/models/losses.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 
 class GPTLoss(nn.Module):
     def __init__(self):
+        super(GPTLoss, self).__init__()
         self.ccl = nn.CrossEntropyLoss()
 
     def forward(self, inputs, targets):
